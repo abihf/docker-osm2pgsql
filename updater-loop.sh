453b6cc03f64faeb66cbe4b2abf5b728dad79a9a
Abi Hafshin Alfarouq <abi@hafs.in>
2016-05-30 16:52:16 +0700

    initial updater implementation

--- updater-loop.sh
+++ updater-loop.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-while true ; do
-        
+db_user=${DB_ENV_POSTGRES_USER:-postgres}
+db_pass=${DB_ENV_POSTGRES_PASSWORD}
+db_name=${DB_ENV_POSTGRES_DB:-$db_user}
+
+db_host=$DB_PORT_5432_TCP_ADDR
+db_port=$DB_PORT_5432_TCP_PORT
+
+mkdir -p /update/new
+mkdir -p /update/add
+mkdir -p /update/processed
+
+update() {
+        osm2pgsql -H $db_host -P $db_port -U $db_user -W $db_pass -d $db_name --slim $@
+}
+
+for f in /update/new/* ; do
+        update "$f"
 done
